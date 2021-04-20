c869486cf192d0e04c4816c555d49afa7b71b05c
PaytonMills <79946395+PaytonMills@users.noreply.github.com>
2021-04-19 21:13:52 -0400

    changed updateQuote function

--- java.js
+++ java.js
@@ -7,20 +7,22 @@ function setup() {
 function getQuote() {
   fetch("https://api.kanye.rest")
     .then(response => response.json())
-    .then(data => updateQuoteSrc(data.file));
+    .then(data => updateQuote(data.text));
 }
 
-function updateQuoteSrc(src) {
+function updateQuote(src) {
   /* Get the quote text element */
-  p.innerText()
+  let p = document.querySelector("p");
+  /* Update its innerText */
+    p.innerText = src
   //let p = document.createTextNode("p");
   /* Update its src*/
-  //p.src = src;
+
 }
 
 /* Test for Appending */
-var tag = document.createElement("p");
+/* var tag = document.createElement("p");
 var text = document.createTextNode("Kanye says this");
 tag.appendChild(text);
 var element = document.getElementById("new");
-element.appendChild(tag);
+element.appendChild(tag); */
