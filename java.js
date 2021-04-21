6d6280c821581ec8a8e8d539d43e2590548d92eb
PaytonMills <79946395+PaytonMills@users.noreply.github.com>
2021-04-21 19:12:55 -0400

    java changes with lecture

--- java.js
+++ java.js
@@ -7,18 +7,18 @@ function setup() {
 function getQuote() {
   fetch("https://api.kanye.rest")
     .then(response => response.json())
-    .then(data => updateQuote(data.text));
+    .then(data => updateQuote(data.quote));
 }
 
-function updateQuote(src) {
+//function updateQuote(src) {
   /* Get the quote text element */
-  let p = document.querySelector("p");
+ // let p = document.querySelector("p");
   /* Update its innerText */
-    p.innerText = src
+  //  p.innerText = src
   //let p = document.createTextNode("p");
   /* Update its src*/
 
-}
+//}
 
 /* Test for Appending */
 /* var tag = document.createElement("p");
@@ -26,3 +26,10 @@ var text = document.createTextNode("Kanye says this");
 tag.appendChild(text);
 var element = document.getElementById("new");
 element.appendChild(tag); */
+
+function updateQuote(src) {
+  //Get quote p element
+  let p = document.querySelector("p");
+  //Update innerText
+  p.innerText = data.quote;
+}
